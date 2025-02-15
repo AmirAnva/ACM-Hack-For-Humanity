210fa858810e2c8b36dfaba252afd2cb996d7448
AmirAnva <amiranva@gmail.com>
2025-02-15 10:44:36 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// This is the entry point of your JavaScript code
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+    
+    // Your code here
+});
